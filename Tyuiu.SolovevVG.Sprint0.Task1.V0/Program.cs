2496e2104dbae7e00b6c6bfe9e9672eb92e8b513
SolovevVG <Tyuiu.SolovevVG@outlook.com>
2023-10-01 17:10:36 +0500

    Task1

---
+++ Tyuiu.SolovevVG.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SolovevVG.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Валерий";
+            int age = 22;
+
+            Console.WriteLine($"Здравствуйте, меня зовут {name}, мне {age} года!");
+            Console.ReadKey();
+        }
+    }
+}
