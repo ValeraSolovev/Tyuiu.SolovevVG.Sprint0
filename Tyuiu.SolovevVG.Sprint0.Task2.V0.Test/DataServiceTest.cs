1d0a11eb85acff60c1f023873342035ee5597b42
SolovevVG <Tyuiu.SolovevVG@outlook.com>
2023-10-01 17:25:03 +0500

    Task2

---
+++ Tyuiu.SolovevVG.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SolovevVG.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.SolovevVG.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Валерий";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Валерий", res);
+        }
+    }
+}
---
+++ Tyuiu.SolovevVG.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SolovevVG.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SolovevVG.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e77216a-56b8-4af5-b19a-ab159339b4eb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
