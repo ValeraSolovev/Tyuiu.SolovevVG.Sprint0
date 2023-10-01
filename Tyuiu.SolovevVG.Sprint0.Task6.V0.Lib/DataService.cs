1be9a831edc0c81e2e3d6a4051ca194d522a3b8a
SolovevVG <Tyuiu.SolovevVG@outlook.com>
2023-10-01 18:29:08 +0500

    Task6

---
+++ Tyuiu.SolovevVG.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SolovevVG.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        public static object AdditionArray(int[] numbers)
+        {
+            var total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total += numbers[i];
+            }
+            return total;
+        }
+
+        public static object SubstracrionArray(int[] numbers)
+        {
+            int total = 0, index = 0;
+            while (index < numbers.Length)
+            {
+                total -= numbers[index];
+                index++;
+            }
+            return total;
+        }
+
+        public static object MultiplicationArray(int[] numbers)
+        {
+            int total = 1, index = 0;
+            do
+            {
+                total *= numbers[index];
+                index++;
+            }
+            while (index < numbers.Length);
+            return total;
+        }
+    }
+}
